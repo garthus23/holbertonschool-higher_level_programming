3fb4cd5834c9302a0e592de0359687f524a3d614
Gregory ARTHUS <garthus@adminux.fr>
2020-12-08 15:01:37 +0000

    add(): format when var

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,10 +5,10 @@ def fizzbuzz():
 
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("Fizzbuzz", end=" ")
+            print("Fizzbuzz".format(), end=" ")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            print("Buzz".format(), end=" ")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz".format(), end=" ")
         else:
-            print(i, end=" ")
+            print("{}".format(i), end=" ")
