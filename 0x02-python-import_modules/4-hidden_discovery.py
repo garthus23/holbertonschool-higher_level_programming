c8b96d73ba0dd1a6c428a39a6a77afc5fe45f766
Gregory ARTHUS <garthus@adminux.fr>
2020-12-10 12:58:11 +0000

    style(): pep1 le bref

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     import hidden_4
 
     for i in dir(hidden_4):
-        if not "__" in i:
+        if "__" not in i:
             print("{:s}".format(i))
-
