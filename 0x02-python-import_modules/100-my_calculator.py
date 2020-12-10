c5d16ee50d3821ab5739614f6a711922147d8b9d
Gregory ARTHUS <garthus@adminux.fr>
2020-12-10 13:00:48 +0000

    style(): pep1 le bref

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,19 +5,19 @@ if __name__ == "__main__":
     from sys import exit
     from calculator_1 import sub, mul, add, div
 
-    operator = {"+" : add, "-" : sub, "*" : mul, "/" : div}
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
-    
+
     for i in operator:
         if i == argv[2]:
-            print("{:d} {:s} {:d} = {:d}".format(a, i, b, operator[i](a,b)))
-            exit (0)
+            print("{:d} {:s} {:d} = {:d}".format(a, i, b, operator[i](a, b)))
+            exit(0)
 
     print("Unknown operator. Available operators: +, -, * and /")
-    exit (1)
+    exit(1)
