3ad9745ad509b62288e5cc187726118765dee801
Gregory ARTHUS <garthus@adminux.fr>
2020-12-15 11:21:37 +0000

    debug(): int list is negative

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
 
     if len(my_list) == 0:
         return (None)
-    result = 0
+    result = my_list[0] 
     for i in my_list:
         if i > result:
             result = i
