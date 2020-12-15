79d35993ebb5ff091119af6e3b5cd28a769ec19b
Gregory ARTHUS <garthus@adminux.fr>
2020-12-15 04:12:51 +0000

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+
+    if (len(my_list) > 0):
+        i = 1
+        while i != len(my_list) + 1:
+            print("{:d}".format(my_list[-i]))
+            i += 1
