15faf38c72010788e850ffa062efc27f467e63be
Gregory ARTHUS <garthus@adminux.fr>
2020-12-15 17:01:27 +0000

    debug(): print value instead of index

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,7 @@
 
 def print_list_integer(my_list=[]):
 
-    i = 1
-    while (i != len(my_list) + 1):
-        print("{:d}".format(i))
+    i = 0
+    while (i != len(my_list)):
+        print("{:d}".format(my_list[i]))
         i += 1
