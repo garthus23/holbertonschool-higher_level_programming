64926994f60f01e253e71bf33e61b835bd73b090
Gregory ARTHUS <garthus@adminux.fr>
2020-12-16 16:32:20 +0000

    debug():None list case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,8 +3,9 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    if (len(my_list) > 0):
-        i = 1
-        while i != len(my_list) + 1:
-            print("{:d}".format(my_list[-i]))
-            i += 1
+    if (my_list is not None):
+        if (len(my_list) > 0):
+            i = 1
+            while i != len(my_list) + 1:
+                print("{:d}".format(my_list[-i]))
+                i += 1
