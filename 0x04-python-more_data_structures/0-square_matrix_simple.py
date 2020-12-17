521801fdeb4a540783bf8807966e6a61af021232
Gregory ARTHUS <garthus@adminux.fr>
2020-12-17 21:09:19 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
 
+def copy_2d(p):
+    return [list(p2) for p2 in p]
+
+
 def square_matrix_simple(matrix=[]):
 
-    newlist = []
-
-    for i in range(5):
-
-    # Append an empty sublist inside the list
-        newlist.append([])
-
-    for j in range(5):
-        newlist[i].append(j)
-
-    print(newlist)
+    array = copy_2d(matrix)
+    i = 0
+    j = 0
+    while i < len(array):
+        while j < len(array[i]):
+            array[i][j] *= array[i][j]
+#            print(array[i][j])
+            j += 1
+        j = 0
+        i += 1
+    return (array)
