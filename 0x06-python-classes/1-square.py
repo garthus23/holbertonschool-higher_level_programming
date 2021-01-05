e76efc52fbc297cb2c2e7694603f1c9c767a1daf
Gregory ARTHUS <garthus@adminux.fr>
2021-01-05 18:01:44 +0100

    add(): google doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
 
+"""
+    class of square
+"""
+
+
 class Square:
+    """
+    Class that defines a square
+
+    Attributes:
+        size (size): this is the size
+    """
+
     def __init__(self, size):
         self.__size = size
