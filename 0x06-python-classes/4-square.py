cb9b1cd47a3e01f986a3659ce30c351d0408e173
Gregory ARTHUS <garthus@adminux.fr>
2021-01-05 20:00:49 +0100

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,7 +27,7 @@ class Square:
                 raise ValueError("size must be >= 0")
             self.__size = size
 
-    size = property(get_size, set_size)
-
     def area(self):
         return (self.__size * self.__size)
+
+    size = property(get_size, set_size)
