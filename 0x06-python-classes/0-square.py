3f11911aebcc2b99ebe22ed97efd609663e09831
Gregory ARTHUS <garthus@adminux.fr>
2021-01-05 17:48:02 +0100

    debug(): doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,18 +2,5 @@
 
 
 class Square:
-    """Class methods are similar to regular functions.
-
-    Note:
-        Do not include the `self` parameter in the ``Args`` section.
-
-    Args:
-        param1: The first parameter.
-        param2: The second parameter.
-
-    Returns:
-        True if successful, False otherwise.
-
-    """
-
+    """Class methods are similar to regular functions."""
     pass
