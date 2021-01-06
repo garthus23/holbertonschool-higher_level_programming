0346c3ba04abadb755692ab0d04b80b8d830982b
Gregory ARTHUS <garthus@adminux.fr>
2021-01-06 19:27:24 +0100

    debug(): add no size scenario

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,19 +13,18 @@ class Square:
     Attributes:
         size(size): this is the size
     """
-
-    def __init__(self, size):
+    def __init__(self, size=0):
         self.set_size(size)
 
     def get_size(self):
         return self.__size
 
-    def set_size(self, size=0):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+    def set_size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         return (self.__size * self.__size)
