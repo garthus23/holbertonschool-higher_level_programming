092942ef85588dec7fd5fcd29a4a52a4958f1e1e
Gregory ARTHUS <garthus@adminux.fr>
2021-01-08 20:23:10 +0100

    task 100

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+"""
+    Matrix multiplication
+    Attributes: m_a(matrix): first matrix m_b(matrix): second matrix
+    Return result(matrix)
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    Multiply two matrix m_a * m_b
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if m_a == [[]] or m_a == []:
+        raise ValueError("m_a can't be empty")
+
+    if m_b == [[]] or m_b == []:
+        raise ValueError("m_b can't be empty")
+    for i in range(len(m_a)):
+        if len(m_a[0]) is not len(m_a[i]):
+            raise TypeError("each row of m_a must be of the same size")
+        for j in range(len(m_a[i])):
+            if not isinstance(m_a[i][j], (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for i in range(len(m_b)):
+        if len(m_b[0]) is not len(m_b[i]):
+            raise TypeError("each row of m_b must be of the same size")
+        for j in range(len(m_b[i])):
+            if not isinstance(m_b[i][j], (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+    if len(m_a) < len(m_b):
+        result = [[0 for x in range(len(m_a[0]))] for y in range(len(m_a))]
+    else:
+        result = [[0 for x in range(len(m_b[0]))] for y in range(len(m_b))]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                try:
+                    result[i][j] += m_a[i][k] * m_b[k][j]
+                except ValueError:
+                    print("m_a and m_b can't be multiplied")
+    return result
