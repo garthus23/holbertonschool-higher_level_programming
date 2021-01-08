45de5314fee08772218ace7781888182461c9e72
Gregory ARTHUS <garthus@adminux.fr>
2021-01-08 05:33:30 +0100

    task 2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""
+    matrix_divided: divide all elements of a matrix
+    Attributes: matrix(list): list of int, div(int): number used to divide
+    Return: new matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    function: divide list of integer
+    """
+    new_matrix = []
+    new_matrix = list(map(list, matrix))
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zeo")
+    for i in range(len(matrix)):
+        if len(matrix[i]) is not len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = round((new_matrix[i][j] / 3), 2)
+
+    return (new_matrix)
