a274276443d4744afc70eae55e5778aa38c6bad5
Gregory ARTHUS <garthus@adminux.fr>
2021-01-08 05:54:08 +0100

    debug: each value must be int or float

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,6 +12,7 @@ def matrix_divided(matrix, div):
     """
     function: divide list of integer
     """
+    mustbeint = "matrix must be a matrix (list of lists) of integers/floats"
     new_matrix = []
     new_matrix = list(map(list, matrix))
 
@@ -23,6 +24,7 @@ def matrix_divided(matrix, div):
         if len(matrix[i]) is not len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for j in range(len(matrix[i])):
+            if not isinstance(matrix[i][j], (int, float)):
+                raise TypeError(mustbeint)
             new_matrix[i][j] = round((new_matrix[i][j] / 3), 2)
-
     return (new_matrix)
