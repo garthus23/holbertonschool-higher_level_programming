f1119ab7779162708be5d6c3fa86ce81c61a3e30
Gregory ARTHUS <garthus@adminux.fr>
2021-01-13 15:59:19 +0100

    debug(): proper error msg

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,9 +29,9 @@ class Rectangle:
     def height(self, height):
         """ setter height """
         if not isinstance(height, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if height < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     @width.setter
