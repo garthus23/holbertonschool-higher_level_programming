06034b0e2b61580eee2a758d0aafd11ac0299dfe
Gregory ARTHUS <garthus@adminux.fr>
2021-01-19 15:49:00 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
+
+"""
+    class of lists
+"""
+
+
 class MyList(list):
+    """ class of list """
     pass
 
     def print_sorted(self):
+        """ sort the list """
         x = self.copy()
         return (x.sort())
