8bcd1522f15426c05b4e5a1182e29cabb9685ef4
Gregory ARTHUS <garthus@adminux.fr>
2021-01-19 16:01:34 +0100

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+    return True if object is an instance
+"""
+
+
+def is_same_class(obj, a_class):
+    """ return true if obj is class """
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
