6d6b1dcc4df7820d24752aa55c5c314146d2a611
Gregory ARTHUS <garthus@adminux.fr>
2021-01-19 16:19:45 +0100

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""
+    object  is an instance of a class that inherited from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ obj == class ? """
+
+    return(isinstance(obj, a_class))
