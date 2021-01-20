20240dcbae67fbe8aff4ecb0e0acb800d91b8489
Gregory ARTHUS <garthus@adminux.fr>
2021-01-20 17:38:42 +0100

    style(): import in good place

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import json
+
 """
     load_from_json_file
 """
@@ -8,6 +8,7 @@ import json
 
 def load_from_json_file(filename):
     """ load_from_json_file """
+    import json
     with open(filename, 'r') as f :
         for line in f:
             return(json.loads(line))
