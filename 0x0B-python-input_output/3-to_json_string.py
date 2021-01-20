b5919ee7d74cef70add0f13b6143bc85abd0e188
Gregory ARTHUS <garthus@adminux.fr>
2021-01-20 17:32:08 +0100

    style(): try import place

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import json
 
 
 """
     JSON string to Object
 """
 
-
 def to_json_string(my_obj):
     """ to_json_string """
+    import json
     return(json.dumps(my_obj))
