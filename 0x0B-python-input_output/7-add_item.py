4768c6f2ac4ff5f1e5366bb5d1ac75b7057732bb
Gregory ARTHUS <garthus@adminux.fr>
2021-01-20 18:55:04 +0100

    add(): half task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,16 @@
 """
 
 
- if __name__ = "__main__":
+if __name__ == "__main__":
+    """ main function """
+    from sys import argv
+    import json
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+    list1 = []
 
+    for i in range(len(argv)):
+        if i > 0:
+            list1.append(argv[i])
+
+    save_to_json_file(list1, 'add_item.json')
