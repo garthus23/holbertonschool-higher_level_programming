3f27d80ad566fea7f0059d5db5839f64f1e05721
Gregory ARTHUS <garthus@adminux.fr>
2021-01-21 09:58:51 +0100

    debug(): load the list from json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,8 +11,16 @@ if __name__ == "__main__":
     from sys import argv
     import json
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    read_file = __import__('0-read_file').read_file
 
     list1 = []
+
+    try : 
+        with open('add_item.json', 'r') as f:
+            for line in f:
+                 list1 = json.loads(line)
+    except:
+        pass
 
     for i in range(len(argv)):
         if i > 0:
