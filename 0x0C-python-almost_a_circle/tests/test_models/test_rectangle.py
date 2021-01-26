5c4fb39c8c4fb9b1d24cbaaef901c5d735ce7fe2
Gregory ARTHUS <garthus@adminux.fr>
2021-01-26 15:28:32 +0100

    add(): more test rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,11 +6,17 @@ class TestsRectangle(unittest.TestCase):
 
     def test_int(self):
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 7)
+        self.assertEqual(r1.id, 6)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
 
     def test_all_args(self):
-        r3 = Rectangle(10, 2, 4, 5)
-        self.assertEqual(r3.id, 4)
+        r3 = Rectangle(10, 2, 4, 5, 6)
+        self.assertEqual(r3.id, 6)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 4)
+        self.assertEqual(r3.y, 5)
 
     def test_str(self):
         self.assertRaises(TypeError, Rectangle, 10, "2")
