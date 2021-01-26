f311e0555c801313d4ff8eb5c8cf6fd95691b38a
Gregory ARTHUS <garthus@adminux.fr>
2021-01-26 15:55:08 +0100

    add(): more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -22,12 +22,20 @@ class TestsRectangle(unittest.TestCase):
         self.assertRaises(ValueError, Rectangle, 0, 0, 0, 0, 0)
 
     def test_str(self):
+
+        self.assertRaises(TypeError, Rectangle, "10")
         self.assertRaises(TypeError, Rectangle, 10, "2")
         self.assertRaises(TypeError, Rectangle, 10, 3, "4")
+        self.assertRaises(TypeError, Rectangle, 10, 2, 3, "6")
+
 
     def test_below_0(self):
+        self.assertRaises(TypeError, Rectangle, -10)
         self.assertRaises(ValueError, Rectangle, 10, -10)
         self.assertRaises(ValueError, Rectangle, 10, 10, -10)
+        self.assertRaises(TypeError, Rectangle, 10, 10, 10, -10, 10, 10)
+        self.assertRaises(TypeError, Rectangle, 10, 10, 10, 10, 10 , -10)
+
 
     def test_below_0_height(self):
         self.assertRaises(ValueError, Rectangle, 10, 9, -2)
