39a2b7b0af4e9a71a85e24ad49b6ffbfc9a34051
Gregory ARTHUS <garthus@adminux.fr>
2021-01-26 13:42:01 +0100

    add(): more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,3 +7,9 @@ class TestClass(unittest.TestCase):
     def test_none(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
