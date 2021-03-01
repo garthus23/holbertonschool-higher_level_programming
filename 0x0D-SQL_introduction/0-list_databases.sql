6941f3c2f6d8e614e759a070d28febd668c4586f
Gregory ARTHUS <garthus@adminux.fr>
2021-03-01 13:56:53 +0000

    task 0 and 1

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- list all databases on the Mysql Server
+-- list all Databases
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create database on the Mysql Server
+-- Create Database hbtn
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
