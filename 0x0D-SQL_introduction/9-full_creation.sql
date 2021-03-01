0239fc27abca45f573cdce8f349b781d7e69e044
Gregory ARTHUS <garthus@adminux.fr>
2021-03-01 17:41:40 +0000

    debug(): rename georges to george

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,4 +8,4 @@ INSERT INTO `second_table`(id,name,score) VALUES (2,'Alex',3);
 -- insert third record
 INSERT INTO `second_table`(id,name,score) VALUES (3,'Bob',14);
 -- insert fourth record
-INSERT INTO `second_table`(id,name,score) VALUES (4,'Georges',8);
+INSERT INTO `second_table`(id,name,score) VALUES (4,'George',8);
