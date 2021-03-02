a54247651b11292b386de206d1b4009767d3d848
Gregory ARTHUS <garthus@adminux.fr>
2021-03-02 11:35:41 +0000

    debug(): change column name to genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Join Database and print selected Columns
 -- Join Database and print selected Columns
-SELECT tv_genres.name, count(tv_genres.id) AS number_of_shows FROM tv_genres RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, count(tv_genres.id) AS number_of_shows FROM tv_genres RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
