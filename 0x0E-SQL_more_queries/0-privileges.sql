35a1128591441906453d833ef8da194df3522de9
Gregory ARTHUS <garthus@adminux.fr>
2021-03-02 14:23:47 +0000

    debug(): remove line

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- list all privileges On mySql Server
 -- list all privileges Of user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- list all privileges Of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
