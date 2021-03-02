0319425e21e693923768c46f6e5cae9802a38f92
Gregory ARTHUS <garthus@adminux.fr>
2021-03-02 15:09:13 +0000

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,3 @@
+-- Join Database and print selected Columns
+-- Join Database and print selected Columns
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id INNER JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_genres.name='Comedy' ORDER BY tv_shows.title ASC;
