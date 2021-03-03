33e5cd8f1d6eb4f70e0f22d5c70f6655d99fc235
Gregory ARTHUS <garthus@adminux.fr>
2021-03-03 09:53:41 +0000

    task 101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,4 @@
+-- Join Database and print selected Columns
+-- Join Database and print selected Columns
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id WHERE
+ tv_genres.name='Comedy') ORDER BY tv_shows.title ASC;
