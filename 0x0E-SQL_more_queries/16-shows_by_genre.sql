e2f47634656b8d4f6d5c66b7ba434c73f9a96a37
Gregory ARTHUS <garthus@adminux.fr>
2021-03-03 08:15:43 +0000

    task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Join Database and print selected Columns
 -- Join Database and print selected Columns
-SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id INNER JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_genres.name='Comedy' ORDER BY tv_shows.title ASC;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id ORDER BY tv_shows.title ASC;
