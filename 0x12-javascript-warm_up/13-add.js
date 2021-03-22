69abca3279f94ae1a89430a873d1b63bf1b3859f
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 17:33:54 +0100

    debug(): undefined number case

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 
 exports.add = function (num1, num2) {
+  if (num1 === undefined && num2 === undefined) {
+      return 0;
+  } else if (num2 === undefined) {
+      return num1;
+  } else if (num1 === undefined) {
+      return num2;
+  }
   if (parseInt(num1) && parseInt(num2)) {
     return (parseInt(num1) + parseInt(num2));
   }
--- 0x12-javascript-warm_up/13-main.js
+++ 0x12-javascript-warm_up/13-main.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 const add = require('./13-add').add;
 console.log(add(3, 5));
+console.log(add(-3, 15));
+console.log(add(undefined, 3));
+
