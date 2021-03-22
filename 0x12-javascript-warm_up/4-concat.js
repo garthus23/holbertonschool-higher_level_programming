3677b534e96c2b13252fdc3d7e9c33ea493ea967
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 12:29:03 +0100

    style(): semi standard compliant

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv[2]) {
-    if (!process.argv[3]){
+    if (!process.argv[3]) {
         console.log(process.argv[2] + ' is undefined');
     } else {
         console.log(process.argv[2] + ' is ' + process.argv[3]);
