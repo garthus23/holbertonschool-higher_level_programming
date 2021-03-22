a9634a0e45e3657d1c28060af62048aeaecd5bbe
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 13:43:39 +0100

    debug(): negative number case

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 let i = 0;
 
-if (process.argv[2] && parseInt(process.argv[2]) > 0 ) {
+if (process.argv[2] && parseInt(process.argv[2]) > 0) {
   while (i !== parseInt(process.argv[2])) {
     console.log('C is fun');
     i += 1;
