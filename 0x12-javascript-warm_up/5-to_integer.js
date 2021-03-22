59a0ec1dddcaafadb9859e654064cbc0caae386f
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 13:03:24 +0100

    debug(): try an other function to convert in int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 if (!process.argv[2]) {
   console.log('Not a number');
 } else {
-  if (Number(process.argv[2])) {
-    console.log('My number : ' + Number(process.argv[2]));
+  if (parseInt(process.argv[2])) {
+    console.log('My number : ' + parseInt(process.argv[2]));
   } else {
     console.log('Not a number');
   }
