31da81992365b7105869c839f57517deb72fb2fc
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 15:25:20 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 let i;
 if (parseInt(process.argv[2]) > 0) {
-  for (i = 0; i != process.argv[2]; i++) {
+  for (i = 0; i < process.argv[2]; i++) {
     console.log('X'.repeat(process.argv[2]));
   }
 } else {
