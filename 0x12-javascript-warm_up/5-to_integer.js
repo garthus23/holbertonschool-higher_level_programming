8e63ade3cf1ee2fb3a43d5afdb55bf5db96aeda3
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 13:00:46 +0100

    debug(): task 5 with correct sentence

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ if (!process.argv[2]) {
   console.log('Not a number');
 } else {
   if (Number(process.argv[2])) {
-    console.log(Number(process.argv[2]));
+    console.log('My number : ' + Number(process.argv[2]));
   } else {
     console.log('Not a number');
   }
