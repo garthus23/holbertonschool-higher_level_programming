c78f65ed02506ed597f2e366c25868864033e5c7
Gregory ARTHUS <garthus@adminux.fr>
2021-03-22 01:50:12 +0100

    task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (process.argv[2]) {
+    if (!process.argv[3]){
+        console.log(process.argv[2] + ' is undefined');
+    } else {
+        console.log(process.argv[2] + ' is ' + process.argv[3]);
+    }
+} else {
+    console.log('undefined is undefined');
+}
