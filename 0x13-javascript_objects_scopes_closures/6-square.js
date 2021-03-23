0fba9136ce0157f24b2dfe37fa9f1cef81cf9764
Gregory ARTHUS <garthus@adminux.fr>
2021-03-23 17:35:40 +0100

    debug(): import rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,33 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (parseInt(w) > 0 && parseInt(h) > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print (c = 'X') {
-    let i;
-    for (i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-module.exports = Rectangle;
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
