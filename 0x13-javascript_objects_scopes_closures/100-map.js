c9f52a8ef1c7843c8252a933eedee3926b87ee24
Gregory ARTHUS <garthus@adminux.fr>
2021-03-23 19:49:21 +0100

    debug(): modify index in map function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const map1 = list.map(x => x * list.indexOf(x));
+const map1 = list.map((value, index) => index * value);
 console.log(list);
 console.log(map1);
