a71debc09963ce1b2a45b3f394dfa7650b3f891b
Gregory ARTHUS <garthus@adminux.fr>
2021-03-23 17:29:13 +0100

    debug(): add the Rectangle class inside the function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,33 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (c = 'X') {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
 
 class Square extends Rectangle {
   constructor (size) {
