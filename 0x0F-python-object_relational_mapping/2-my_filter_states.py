aabae7f2e81256dda9d231a443f13eb25b770f17
Gregory ARTHUS <garthus@adminux.fr>
2021-03-25 21:53:01 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    argnum = len(sys.argv)
+    if (argnum == 5):
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3],
+                               charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE '%s' ORDER \
+                     BY states.id ASC" % (sys.argv[4]))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
