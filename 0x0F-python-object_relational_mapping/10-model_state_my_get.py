595e01c3e4cd9049b5ab69e922d787eec8c39a5b
Gregory ARTHUS <garthus@adminux.fr>
2021-03-26 15:10:09 +0100

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = (session.query(State.id).
+             filter(State.name.like(sys.argv[4])).first())
+
+    if query:
+        print(query[0])
+    else:
+        print('Not found')
