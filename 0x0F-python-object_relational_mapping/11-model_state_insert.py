b95a5a56ed521da998d01aed55aeed08d258a658
Gregory ARTHUS <garthus@adminux.fr>
2021-03-26 17:03:01 +0100

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    c1 = State(name='Louisiana')
+    session.add(c1)
+    session.commit()
+    query = (session.query(State.id).
+             filter(State.name.like('Louisiana')).first())
+    if query:
+        print(query[0])
+    else:
+        print('Not found')
