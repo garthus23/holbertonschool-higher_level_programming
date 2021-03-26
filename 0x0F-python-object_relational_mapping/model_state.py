e6daef91b1c46ac1d319196a75f3e77857dbbaf3
Gregory ARTHUS <garthus@adminux.fr>
2021-03-27 00:58:37 +0100

    style(): doc added

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-
+"""
+    module sys anf mysqld
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+"""
+    class State: it's where we store american states
+"""
+
 
 class State(Base):
+    """ class state """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128))
+    name = Column(String(128))
 
     def __repr__(self):
+        """ method to convert obj in str """
         return "{}: {}".format(self.id, self.name)
