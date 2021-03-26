8a70131fd96920b467f401ceab8dfb3e148cbea1
Gregory ARTHUS <garthus@adminux.fr>
2021-03-27 00:32:52 +0100

    first letter only Uppercase case

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2], db=sys.argv[3],
                                charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER \
+        cur.execute("SELECT * FROM states WHERE name REGEXP BINARY '^N' ORDER \
                      BY states.id ASC")
         query_rows = cur.fetchall()
         for row in query_rows:
