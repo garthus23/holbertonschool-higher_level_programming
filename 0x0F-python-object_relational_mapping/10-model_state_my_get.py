37d8ad2fd4ec0e212992f03757e626192227d2c0
Gregory ARTHUS <garthus@adminux.fr>
2021-03-27 01:17:35 +0100

    style(): doc added

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
+"""
+    module sys anf mysqld
+"""
 import sys
 from model_state import Base, State
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+"""
+    prints the State object with the name passed as argument
+"""
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
