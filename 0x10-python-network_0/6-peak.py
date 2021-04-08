315011131ad330bccbe9ce7e5979382ff22399fc
Gregory <garthus@adminux.fr>
2021-04-08 17:59:17 +0200

    Pep guardiola

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-# find the peak on a list
+
+"""
+	find the peak
+"""
 
 def find_peak(list_of_integers):
-    ''' find the max on a list '''
+    """ find the max on a list """
     if list_of_integers:
         list_of_integers.sort()
         return list_of_integers[-1]
