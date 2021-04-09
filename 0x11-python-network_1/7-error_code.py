77f1a2997eed63fd80600cb207185d0a0da72fd7
Gregory <garthus@adminux.fr>
2021-04-09 13:06:32 +0200

    style(): pep8 compliant

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 
 
-
 """ module urllib """
 import requests
 import sys
 
 
-
-""""
-	script that fetches an url
+"""
+    script that fetches an url
 """
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) == 2:
         response = requests.get(sys.argv[1])
         if response.status_code >= 400:
             print("Error code: {}".format(response.status_code))
-        else:        
+        else:
             print("{}".format(response.text))
