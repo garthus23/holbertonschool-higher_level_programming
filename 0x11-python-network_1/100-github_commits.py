2a91291afda82bb457a4950020bfd899dc90952f
Gregory <garthus@adminux.fr>
2021-04-09 05:48:28 +0200

    add(): to finish maybe

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+""" module to request """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2])
+        login = requests.get(url).json()
+        try:
+            print(login)
+        except KeyError:
+            print('None')
+    else:
+        print('None')
