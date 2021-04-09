c3e239bf5bd2f0d0671175e3f39dc36cfd2f5b7f
Gregory <garthus@adminux.fr>
2021-04-09 05:04:38 +0200

    task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+""" module to request """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        token = sys.argv[2]
+        login = requests.get('https://api.github.com/user', auth=(username,token)).json()
+        try:
+            print(login['id'])
+        except KeyError:
+            print('None')
+    else:
+        print('None')
