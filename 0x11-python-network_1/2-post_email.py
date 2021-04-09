d5367d85258c2a75076f995c0a1b8975b122ebce
Gregory <garthus@adminux.fr>
2021-04-09 12:33:15 +0200

    style(): pep8 compliant

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 3:
         data = {
-        'email': sys.argv[2]
+                'email': sys.argv[2]
         }
         data = bytes(urllib.parse.urlencode(data).encode())
-        with urllib.request.urlopen(sys.argv[1], data)  as response:
-            print(response.read().decode('utf-8'));
+        with urllib.request.urlopen(sys.argv[1], data) as response:
+            print(response.read().decode('utf-8'))
