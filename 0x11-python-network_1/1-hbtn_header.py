dfaf8ad3def22f02773adf654ac2361c0e97b9b2
Gregory <garthus@adminux.fr>
2021-04-09 12:24:23 +0200

    style(): pep style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -13,6 +13,6 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) == 2:
-        with urllib.request.urlopen(sys.argv[1])  as response:
-            header=dict(response.info())
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            header = dict(response.info())
             print(header['X-Request-Id'])
