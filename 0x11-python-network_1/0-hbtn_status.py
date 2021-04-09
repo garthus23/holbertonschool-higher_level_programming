bba4042d1288e892d0ed9e7478a6ffb52484d683
Gregory <garthus@adminux.fr>
2021-04-09 12:22:05 +0200

    style(): pep compliant

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-
-
-
 """ module urllib """
 import urllib.request
 
 
-
 """"
-	script that fetches an url
+     script that fetches an url
 """
 
 
 if __name__ == "__main__":
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status')  as response:
-        data = response.read() 
+    with urllib.request.urlopen('https://intranet.hbtn.io/status')\
+            as response:
+        data = response.read()
 
     if data:
         print("Body response:")
