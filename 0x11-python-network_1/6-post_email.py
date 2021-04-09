df91ca47346f47261a33e1658ec6c857737bb1f4
Gregory <garthus@adminux.fr>
2021-04-09 04:01:17 +0200

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+""" module urllib """
+import requests
+import sys
+
+
+""""
+	script that fetches an url
+"""
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+        print(response.text)
