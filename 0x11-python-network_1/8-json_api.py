3701cfc5c310bf7e6ec8e5e68ad9a94e84829776
Gregory <garthus@adminux.fr>
2021-04-09 13:11:48 +0200

    style(): pep8 compliant

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,14 +13,15 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': sys.argv[1]}).json()
+        response = requests.post('http://0.0.0.0:5000/search_user',
+                                 data={'q': sys.argv[1]}).json()
         if response:
             if response['id'] and response['name']:
-                print("[{}] {}".format(response['id'], response['name']))
+                print("[{}] {}".format(response.get('id'),
+                                       response.get('name')))
             else:
                 print('Not a valid JSON')
         else:
             print('No result')
     else:
         print('No result')
-
